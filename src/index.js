31bac06640dcdff29da58cd392e2885ed5293017
Anthony Curtis <tonydc1997@gmail.com>
2018-04-15 16:12:04 -0400

    Finish creating 'Hello' React component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={`Hello React Ninja!`} />, document.getElementById('root'));
 registerServiceWorker();
