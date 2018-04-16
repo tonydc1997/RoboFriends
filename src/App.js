6061524fbf5453837512ab950dcd58270c449cf2
Anthony Curtis <tonydc1997@gmail.com>
2018-04-16 12:15:14 -0400

    Test SearchBox functionality

--- src/App.js
+++ src/App.js
@@ -11,11 +11,16 @@ class App extends Component {
       searchField: ''
     }
   }
+
+onSearchChange(event) {
+  console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className='tc'>
         <h1>RoboFriends</h1>
-        <SearchBox />
+        <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={this.state.robots}/>
       </div>
     );
