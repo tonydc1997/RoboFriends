c4bee966bdbc26d6196f0f181122a4f208ea6b23
Anthony Curtis <tonydc1997@gmail.com>
2018-04-17 06:50:43 -0400

    Add control flow when fetching users

--- src/App.js
+++ src/App.js
@@ -27,13 +27,17 @@ onSearchChange = (event) => {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     })
-    return (
-      <div className='tc'>
-        <h1 className='f2'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots}/>
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f2'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots}/>
+        </div>
+      );
+    }
   }
 }
 
