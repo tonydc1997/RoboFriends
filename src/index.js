472d8813fe063c3a1c27067fda1d3bce5af3ec12
Anthony Curtis <tonydc1997@gmail.com>
2018-08-03 08:19:15 -0700

    Import function from redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
