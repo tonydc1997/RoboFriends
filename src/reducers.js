b3ccf97c4d69c7c1d33e65a34b40e000c10331eb
Anthony Curtis <tonydc1997@gmail.com>
2018-08-03 07:59:56 -0700

    Import constant

--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,11 @@
+import { CHANGE_SEARCH_FIELD } from './constants.js'
+
 const initialState = {
   searchField: ''
 }
 
 export const searchRobots = (state=initialState, action={}) => {
-  
+  switch(action.type) {
+
+  }
 }
