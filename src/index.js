5545afd582f2a7753492e6bd21fee3c8cfd52757
Anthony Curtis <tonydc1997@gmail.com>
2018-08-03 08:25:39 -0700

    Create store and import a reducer

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
+
+const store = createStore(searchRobots);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
