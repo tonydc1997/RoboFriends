f86ec25de2d52d88736114d4510d6022b09bd23e
Anthony Curtis <tonydc1997@gmail.com>
2018-08-03 08:12:49 -0700

    Import Provider and connect from react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
