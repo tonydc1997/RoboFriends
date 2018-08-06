7a7f7ca1b6f875853404814e770a355db6d2fac1
Anthony Curtis <tonydc1997@gmail.com>
2018-08-06 09:00:45 -0700

    Remove redundant code and update function

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,17 +25,9 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: []
-    }
-  }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots();
   }
 
 
