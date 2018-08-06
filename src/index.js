4ee1171e944a26129c418154f497b6c14f5b41c0
Anthony Curtis <tonydc1997@gmail.com>
2018-08-06 07:40:11 -0700

    Use applyMiddleware()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
@@ -10,7 +10,7 @@ import { searchRobots } from './reducers';
 import 'tachyons';
 
 const logger = createLogger;
-const store = createStore(searchRobots);
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store} >
