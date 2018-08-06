b062e42064faa0d8c0b91f0168de182a043bcf67
Anthony Curtis <tonydc1997@gmail.com>
2018-08-06 07:40:58 -0700

    Fix minor error

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { searchRobots } from './reducers';
 import 'tachyons';
 
-const logger = createLogger;
+const logger = createLogger();
 const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
