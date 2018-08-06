2fed798855b92a1a0d38a38061b794c481e99017
Anthony Curtis <tonydc1997@gmail.com>
2018-08-06 08:37:53 -0700

    Differentiate initial states

--- src/reducers.js
+++ src/reducers.js
@@ -5,11 +5,15 @@ import {
   REQUEST_ROBOTS_FAILED  
 } from './constants.js';
 
-const initialState = {
+const initialStateSearch = {
   searchField: ''
 }
 
-export const searchRobots = (state=initialState, action={}) => {
+const initialStateRobots = {
+  
+}
+
+export const searchRobots = (state=initialStateSearch, action={}) => {
   switch(action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, {searchField: action.payload});
