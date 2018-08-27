92412c178b60376685df416842f9e8f13cc9a06a
Anthony Curtis <tonydc1997@gmail.com>
2018-08-27 10:37:06 -0700

    Compress robot images to smaller size

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({name, email, id}) => {
   return(
     <div className=' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}?set=set3&200x200`} alt="Robot"/>
+      <img src={`https://robohash.org/${id}/set_set3?size=200x200`} alt="Robot"/>
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
