7ff009e3ccbaba174536a43faf31d7b60eef7a1c
Anthony Curtis <tonydc1997@gmail.com>
2018-08-28 08:45:33 -0700

    Re-order import statements

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { setSearchField, requestRobots } from '../actions';
+
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
-
-import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = (state) => {
   return {
