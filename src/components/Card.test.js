664ad70c44b9e42292b803f7d495d456d819450e
Anthony Curtis <tonydc1997@gmail.com>
2018-09-01 10:09:23 -0700

    Remove redundant code and test snapshot

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { shallow } from 'enzyme';
 import Card from './Card';
 
 it('Expect to render card component', () => {
-  expect(shallow(<Card />).length).toEqual(1);
+  expect(shallow(<Card />)).toMatchSnapshot();
 })
 
