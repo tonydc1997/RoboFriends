951bd7c2edf799034801107bea24600b2909b1e9
Anthony Curtis <tonydc1997@gmail.com>
2018-09-01 09:38:07 -0700

    Setup failing test

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,5 +1,8 @@
+import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import Card from './Card';
 
-
+it('Expect to render card component', () => {
+  expect(shallow(<Card />).length).toEqual(4);
+})
 
