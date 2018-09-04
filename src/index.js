d2488eeb5587bf17f64f2235301f1b8f6f9c4c21
Anthony Curtis <tonydc1997@gmail.com>
2018-09-04 08:40:14 -0700

    Comment out some code and add console.logs for debugging

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
@@ -10,7 +10,7 @@ import registerServiceWorker from './registerServiceWorker';
 import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
-const logger = createLogger();
+// const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
