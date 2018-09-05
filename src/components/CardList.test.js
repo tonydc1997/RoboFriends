fc3b7930c6ed0203ae43c20b681eda46bed14c81
Anthony Curtis <tonydc1997@gmail.com>
2018-09-05 09:55:58 -0700

    Make CardList.test more readable

--- src/components/CardList.test.js
+++ src/components/CardList.test.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import CardList from './CardList';
 
-it('CardList: render CardList component', () => {
-  const mockRobots = [
-    {
-      id: 1,
-      name: 'Ham Sarris',
-      email: 'hamSarris@gmail.com',
-      key: '2341234'
-    }
-  ]
-  expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
-})
-
+describe('CardList component', () => {
+  it('CardList: renders CardList component', () => {
+    const mockRobots = [
+      {
+        id: 1,
+        name: 'Ham Sarris',
+        email: 'hamSarris@gmail.com',
+        key: '2341234'
+      }
+    ]
+    expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
+  });
+  
+});
