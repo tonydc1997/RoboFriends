bbba2401458aa0023e34c7fdf534cc0d71b52b5e
Anthony Curtis <tonydc1997@gmail.com>
2018-09-06 08:53:46 -0700

    Create reusable variables

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -3,8 +3,20 @@ import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import App from './App';
 
+const mockStore = configureStore();
+
 describe('App container', () => {
+
+  let wrapper, store; 
+
+  beforeEach(() => {
+    store = mockStore(1);
+    store.dispatch = jest.fn();
+    wrapper = shallow(<App store={store}/>);
+  });
+
   it('App: renders correctly', () => {
     expect(shallow(<App />)).toMatchSnapshot();
   });
+
 });
