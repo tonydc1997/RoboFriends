85b81f7ecbcd040252b8b52507dfc1f900c4a72d
Anthony Curtis <tonydc1997@gmail.com>
2018-09-06 08:28:53 -0700

    Write failing test

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -3,5 +3,7 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 describe('App container', () => {
-
+  it('App: renders correctly', () => {
+    expect(shallow(<App />)).toMatchSnapshot();
+  });
 });
