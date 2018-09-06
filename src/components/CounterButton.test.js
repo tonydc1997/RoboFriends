b33a442ef0d89fa39cef897ff4e927dbdf44ddef
Anthony Curtis <tonydc1997@gmail.com>
2018-09-06 08:34:11 -0700

    Refactor code for reabability and simplicity

--- src/components/CounterButton.test.js
+++ src/components/CounterButton.test.js
@@ -3,23 +3,29 @@ import { shallow } from 'enzyme';
 import CounterButton from './CounterButton';
 
 describe('CounterButton component', () => {
+
+  let wrapper, instance;
+
+  beforeEach(() => {
+    wrapper = shallow(<CounterButton initialCountValue={1}/>);
+    instance = wrapper.instance();
+  });
+
   it('Counter: renders correctly', () => {
-    expect(shallow(<CounterButton />)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
+
   it('shouldComponentUpdate: should set the initialCountValue to state', () => {
-    const shallowCounter = shallow(<CounterButton initialCountValue={1} />);
-    expect(shallowCounter.instance().state.count).toBe(1);
+    expect(wrapper.instance().state.count).toBe(1);
   });
+
   it('updateCount: should update count by grabbing current state and incrementing by 1', () => {
-    const shallowCounter = shallow(<CounterButton initialCountValue={1} />);
-    const instance = shallowCounter.instance();
     expect(instance.state.count).toBe(1);
     instance.updateCount();
     expect(instance.state.count).toBe(2);
   });
+
   it('shouldComponentUpdate: should not update count when nextState equals current state', () => {
-    const shallowCounter = shallow(<CounterButton initialCountValue={1} />);
-    const instance = shallowCounter.instance();
     expect(instance.state.count).toBe(1);
     expect(instance.shouldComponentUpdate(null, 1)).toBe(false);
   });
