5d4470249b74f33d3fecda8f9082ceaa2c87cd35
Anthony Curtis <tonydc1997@gmail.com>
2018-09-06 09:19:07 -0700

    Remove mapState and mapDispatch to props

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -7,20 +7,6 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import Header from '../components/Header';
 import './App.css';
 
-const mapStateToProps = (state) => {
-  return {
-    searchField: state.searchRobots.searchField,
-    robots: state.requestRobots.robots,
-    isPending: state.requestRobots.isPending
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  }
-}
 
 class App extends Component {
 
