e9dbedae6bf5225586c6d388c6092ca7d3b47d37
Anthony Curtis <tonydc1997@gmail.com>
2018-09-12 09:55:52 -0700

    Remove unused code

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -21,7 +21,7 @@ class MainPage extends Component {
   }
 
   render() {
-    const { onSearchChange, robots, isPending } = this.props;
+    const { onSearchChange, isPending } = this.props;
     
     return (
         <div className='tc'>
