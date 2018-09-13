86fa789d11880a69dda7fa205099337fb7fc6e6f
Anthony Curtis <tonydc1997@gmail.com>
2018-09-13 12:30:45 -0700

    Write passing test

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -4,4 +4,12 @@ import Header from './Header';
 
 describe('Header Component', () => {
 
+  it('Header: should return a div, h1, and button', () => {
+    const headerComponent = shallow(<Header />).html();
+    expect(headerComponent).toContain('div');
+    expect(headerComponent).toContain('h1');
+    expect(headerComponent).toContain('button');
+  });
+
+
 });
