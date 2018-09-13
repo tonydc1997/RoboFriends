f15f08c841e9205ce45f8ae1d57a8bb304ea75d9
Anthony Curtis <tonydc1997@gmail.com>
2018-09-13 09:48:47 -0700

    Update snapshot and make code efficient

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -4,9 +4,10 @@ import MainPage from './MainPage';
 
 describe('MainPage component', () => {
 
-  let wrapper, instance;
+  let wrapper, instance, mockProps;
+
   beforeEach(() => {
-    const mockProps = {
+    mockProps = {
       onRequestRobots: jest.fn(),
       robots: [],
       searchField: '',
@@ -25,7 +26,7 @@ describe('MainPage component', () => {
   });
 
   it('filterRobots: renders correctly when array is filled', () => {
-    const mockProps1 = {
+    mockProps = {
       onRequestRobots: jest.fn(),
       robots: [{
         id: 1,
@@ -36,8 +37,8 @@ describe('MainPage component', () => {
       searchField: '',
       isPending: false
     }
-    const wrapper2 = shallow(<MainPage { ...mockProps1 } />);
-    expect(wrapper2.instance().filterRobots()).toEqual([{
+    wrapper = shallow(<MainPage { ...mockProps } />);
+    expect(wrapper.instance().filterRobots()).toEqual([{
       id: 1,
       name: 'Bruce Brenner',
       username: 'Bruce',
@@ -46,10 +47,13 @@ describe('MainPage component', () => {
   });
 
   it('isPending: renders cardList / filteredRobots when true', () => {
-
+    expect();
   });
 
   it('isPending: renders <h1>Loading</h1> when true', () => {
+    const mockIsPending = {
+
+    }
 
   });
 
