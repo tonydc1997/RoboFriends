f7c4f3e6c4bc6e4cd68c6f179b8800c85631a797
Anthony Curtis <tonydc1997@gmail.com>
2018-09-13 12:45:29 -0700

    Create tests and a variable

--- src/components/ErrorBoundary.test.js
+++ src/components/ErrorBoundary.test.js
@@ -3,5 +3,15 @@ import { shallow } from 'enzyme';
 import ErrorBoundary from './ErrorBoundary';
 
 describe('ErrorBoundary Component', () => {
-  
+
+  const errorBoundaryComponent = shallow(<ErrorBoundary />);
+
+  it('ErrorBoundary: returns error message if componentDidCatch an error', () => {
+
+  });
+
+  it('ErrorBoundary: returns robots / CardList if componentDid *Not* Catch an error', () => {
+
+  });
+
 });
