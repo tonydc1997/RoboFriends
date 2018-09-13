fc5037ba03aaabf1a343cf70858b07de96af5bcd
Anthony Curtis <tonydc1997@gmail.com>
2018-09-13 10:30:18 -0700

    Comment out code and write tet

--- src/components/Scroll.test.js
+++ src/components/Scroll.test.js
@@ -3,26 +3,30 @@ import { shallow } from 'enzyme';
 import Scroll from 'Scroll';
 
 describe('Scroll Component', () => {
-  
-  it('isPending: renders properly and returns cardList / filteredRobots when false', () => {
-    expect(mockProps.isPending).toEqual(false);
-    fdsdfb
-    // Work on return assertion and decide if 
-  });
 
-
-  it('isPending: renders properly and returns <h1>Loading</h1> when true', () => {
-    mockProps = {
-      onRequestRobots: jest.fn(),
-      robots: [],
-      searchField: '',
-      isPending: true
-    }
-
-    expect(mockProps.isPending).toEqual(true);
-    expect(afgsf)
+  it('Scroll: renders a div', () => {
 
   });
-
   
+  // it('isPending: renders properly and returns cardList / filteredRobots when false', () => {
+  //   expect(mockProps.isPending).toEqual(false);
+  //   fdsdfb
+  //   // Work on return assertion and decide if 
+  // });
+
+
+  // it('isPending: renders properly and returns <h1>Loading</h1> when true', () => {
+  //   mockProps = {
+  //     onRequestRobots: jest.fn(),
+  //     robots: [],
+  //     searchField: '',
+  //     isPending: true
+  //   }
+
+  //   expect(mockProps.isPending).toEqual(true);
+  //   expect(afgsf)
+
+  // });
+
+
 });
