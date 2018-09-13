ac13c3bec799192188168e311c96a9eac9d5ca43
Anthony Curtis <tonydc1997@gmail.com>
2018-09-13 10:28:03 -0700

    Remove isPending tests to scroll component

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -44,24 +44,6 @@ describe('MainPage component', () => {
       email: 'bb@gmail.com'
     }]);
   });
-
-  it('isPending: renders properly and returns cardList / filteredRobots when false', () => {
-    expect(mockProps.isPending).toEqual(false);
-    fdsdfb
-  });
-
-
-  it('isPending: renders properly and returns <h1>Loading</h1> when true', () => {
-    mockProps = {
-      onRequestRobots: jest.fn(),
-      robots: [],
-      searchField: '',
-      isPending: true
-    }
-
-    expect(mockProps.isPending).toEqual(true);
-    expect(afgsf)
-
-  });
+  
 
 });
