9efb0af23a812059f1103900562bf0e79a7581e8
Anthony Curtis <tonydc1997@gmail.com>
2018-09-14 09:39:24 -0700

    Delete previous test ideas

--- src/components/ErrorBoundary.test.js
+++ src/components/ErrorBoundary.test.js
@@ -5,24 +5,4 @@ import ErrorBoundary from './ErrorBoundary';
 describe('ErrorBoundary Component', () => {
 
 
-  it('ErrorBoundary: renders componentDidCatch', () => {
-    const mockComponentDidCatch = jest.fn();
-    const errorBoundaryComponent = shallow(<ErrorBoundary componentDidCatch={mockComponentDidCatch} />);
-
-    expect(errorBoundaryComponent.html()).toBe('');
-  });
-
-  // it('componentDidCatch: should setState for hasError to true if there is an error', () => {
-  //   const errorBoundaryComponent = shallow(<ErrorBoundary />);
-  //   expect(errorBoundaryComponent.instance().componentDidCatch).toBe('');
-  // });
-
-  // it('ErrorBoundary: returns error message if componentDidCatch an error', () => {
-
-  // });
-
-  // it('ErrorBoundary: returns robots / CardList if componentDid *Not* Catch an error', () => {
-
-  // });
-
 });
