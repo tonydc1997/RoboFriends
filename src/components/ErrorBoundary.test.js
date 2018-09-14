d8eb6bb6c98f55c739cffef34381a7f336c66988
Anthony Curtis <tonydc1997@gmail.com>
2018-09-14 09:41:11 -0700

    Create new test

--- src/components/ErrorBoundary.test.js
+++ src/components/ErrorBoundary.test.js
@@ -4,5 +4,8 @@ import ErrorBoundary from './ErrorBoundary';
 
 describe('ErrorBoundary Component', () => {
 
+  it('ErrorBoundary: renders child component when no errors are found', () => {
+
+  });
 
 });
