20d12ad1e31f865eaa2e15f6bfe182148937410c
Anthony Curtis <tonydc1997@gmail.com>
2018-09-14 10:15:58 -0700

    Move some tests into specifc describe block

--- src/components/ErrorBoundary.test.js
+++ src/components/ErrorBoundary.test.js
@@ -17,18 +17,34 @@ describe('ErrorBoundary Component', () => {
     expect(errorBoundaryComponent.find('h3').exists()).toBeTruthy();
   });
 
-  it('ErrorBoundary: should update state to indicate an error if it exists', () => {
-    errorBoundaryComponent.instance().componentDidCatch('Ooops, there was an error');
-    errorBoundaryComponent.update();
-
-    expect(errorBoundaryComponent.instance().state.hasError).toBeTruthy();
+  describe('When error is detected', () => {
+    it('ErrorBoundary: should update state to indicate an error if it exists', () => {
+      errorBoundaryComponent.instance().componentDidCatch('Ooops, there was an error');
+      errorBoundaryComponent.update();
+  
+      expect(errorBoundaryComponent.instance().state.hasError).toBeTruthy();
+    });
+  
+    it('ErrorBoundary: should not render child component when error is detected', () => {
+      errorBoundaryComponent.instance().componentDidCatch('Ooops, there was an error');
+      errorBoundaryComponent.update();
+  
+      expect(errorBoundaryComponent.find('h3').exists()).toBe('false');
+    });
   });
 
-  it('ErrorBoundary: should not render child component when error is detected', () => {
-    errorBoundaryComponent.instance().componentDidCatch('Ooops, there was an error');
-    errorBoundaryComponent.update();
+  // it('ErrorBoundary: should update state to indicate an error if it exists', () => {
+  //   errorBoundaryComponent.instance().componentDidCatch('Ooops, there was an error');
+  //   errorBoundaryComponent.update();
 
-    expect(errorBoundaryComponent.find('h3').exists()).toBe('false');
-  });
+  //   expect(errorBoundaryComponent.instance().state.hasError).toBeTruthy();
+  // });
+
+  // it('ErrorBoundary: should not render child component when error is detected', () => {
+  //   errorBoundaryComponent.instance().componentDidCatch('Ooops, there was an error');
+  //   errorBoundaryComponent.update();
+
+  //   expect(errorBoundaryComponent.find('h3').exists()).toBe('false');
+  // });
 
 });
