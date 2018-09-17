e62d79ddf05e45dcbd534e26b855a1d3cf961539
Anthony Curtis <tonydc1997@gmail.com>
2018-09-17 11:11:26 -0700

    Import constants

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,4 +1,10 @@
 import * as actions from './actions';
+import { 
+  CHANGE_SEARCH_FIELD, 
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS, 
+  REQUEST_ROBOTS_FAILED  
+} from './constants.js';
 
 describe('setSearchField action', () => {
   it('should create an action to search for robots', () => {
