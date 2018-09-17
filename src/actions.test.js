de567ede57046a2b276405e41516d240834fd60d
Anthony Curtis <tonydc1997@gmail.com>
2018-09-17 11:11:11 -0700

    Create new test

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,7 +1,9 @@
 import * as actions from './actions';
 
 describe('setSearchField action', () => {
-  
+  it('should create an action to search for robots', () => {
+    
+  });
 });
 
 describe('requestRobots action', () => {
