89e980cfd873ef8c4cee7af941c27175d770f7a2
Anthony Curtis <tonydc1997@gmail.com>
2018-09-17 10:11:00 -0700

    Import necessary features for test

---
+++ src/reducers.test.js
@@ -0,0 +1,10 @@
+import { 
+  CHANGE_SEARCH_FIELD, 
+  REQUEST_ROBOTS_PENDING,
+  REQUEST_ROBOTS_SUCCESS, 
+  REQUEST_ROBOTS_FAILED  
+} from './constants.js';
+
+import * as reducers from './reducers';
+
+
