13238a0f698267d760e4efb77b6780b934731717
Anthony Curtis <tonydc1997@gmail.com>
2018-09-17 11:29:30 -0700

    Update apiCall

--- src/actions.js
+++ src/actions.js
@@ -12,9 +12,8 @@ export const setSearchField = (text) => ({
 })
 
 export const requestRobots = () => (dispatch) => {
-  dispatch({ type: REQUEST_ROBOTS_PENDING });
-  fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
+  dispatch({ type: REQUEST_ROBOTS_PENDING })
+  apiCall('https://jsonplaceholder.typicode.com/users')
     .then(data => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
     .catch(error => dispatch({ type: REQUEST_ROBOTS_FAILED, payload: error }))
 }
