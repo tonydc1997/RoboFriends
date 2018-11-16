e725e4767f34f66747638c421b6e11544ee7affa
Anthony Curtis <tonydc1997@gmail.com>
2018-11-16 11:31:21 -0800

    style: Have eslint fix style issues on App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,20 +5,16 @@ import { setSearchField, requestRobots } from "../actions";
 import MainPage from "../components/MainPage";
 import "./App.css";
 
-const mapStateToProps = state => {
-  return {
-    searchField: state.searchRobots.searchField,
-    robots: state.requestRobots.robots,
-    isPending: state.requestRobots.isPending
-  };
-};
+const mapStateToProps = state => ({
+  searchField: state.searchRobots.searchField,
+  robots: state.requestRobots.robots,
+  isPending: state.requestRobots.isPending
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value)),
-    onRequestRobots: () => dispatch(requestRobots())
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  onSearchChange: event => dispatch(setSearchField(event.target.value)),
+  onRequestRobots: () => dispatch(requestRobots())
+});
 
 class App extends Component {
   render() {
