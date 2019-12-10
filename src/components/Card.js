ca1739f3a372260bc4803137670c5a3137aea009
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 13:34:38 +0100

    style: Ensure Card(s) go with background color

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const Card = ({name, email, id}) => {
-  return(
-    <div className=' tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img src={`https://robohash.org/${id}/set_set3?size=200x200`} alt="Robot"/>
+const Card = ({ name, email, id }) => {
+  return (
+    <div
+      className="tc white dib br3 pa3 ma2 grow bw2 shadow-5"
+      style={{
+        background:
+          'linear-gradient(to bottom, #5679f9, #5679f9, #5679f9, #5679f9, #5679f9)',
+      }}
+    >
+      <img
+        src={`https://robohash.org/${id}/set_set3?size=200x200`}
+        alt="Robot"
+      />
       <div>
         <h2>{name}</h2>
-        <p>{email}</p>
+        <p className="light-gray">{email}</p>
       </div>
     </div>
   );
-
-}
+};
 
 export default Card;
