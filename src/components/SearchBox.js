af3bdf13b5e8e3cf4ad23a6c182ee94122d1de35
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 13:41:07 +0100

    style: Ensure searchbox border is transparent

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
-    <div className='pa2'>
-      <input 
-      aria-label="Search Robots"
-      className='pa3 ba b--green bg-lightest-blue br2'
-      type="search" 
-      placeholder="search robots"
-      onChange={searchChange}
+    <div className="pa2">
+      <input
+        aria-label="Search Robots"
+        className="pa3 ba b--transparent bg-lightest-blue br2"
+        type="search"
+        placeholder="search robots"
+        onChange={searchChange}
       />
     </div>
   );
-}
+};
 
 export default SearchBox;
