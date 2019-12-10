531e0261a51070fd5c3c1b8731f97fe88e3ece9e
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 14:16:11 +0100

    feat: Move Card styles into its own folder

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,9 @@
 import React from 'react';
+import './Card.css';
 
 const Card = ({ name, email, id }) => {
   return (
-    <div
-      className="tc white w-20 dib br3 pa3 ma2 grow bw2 shadow-5"
-      style={{
-        background:
-          'linear-gradient(to bottom, #5679f9, #5679f9, #5679f9, #5679f9, #5679f9)',
-      }}
-    >
+    <div className="tc white w-20 dib br3 pa3 ma2 grow bw2 shadow-5 bg-purple-gradient">
       <img
         src={`https://robohash.org/${id}/set_set3?size=200x200`}
         alt="Robot"
