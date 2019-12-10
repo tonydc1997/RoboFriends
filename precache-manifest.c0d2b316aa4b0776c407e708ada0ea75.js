5b98ceeee7c7fb19936eb265b7b1e2f9bedf5d7f
Anthony Curtis <tonydc1997@gmail.com>
2019-12-10 16:02:36 +0100

    Updates

---
+++ precache-manifest.c0d2b316aa4b0776c407e708ada0ea75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2226daa155340ac24e91109269fcc262",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "0fcf6fcce7010c2865a4",
+    "url": "/RoboFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8fa7a9693057c3426a69",
+    "url": "/RoboFriends/static/css/main.ebafccc6.chunk.css"
+  },
+  {
+    "revision": "0fcf6fcce7010c2865a4",
+    "url": "/RoboFriends/static/js/2.33001947.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/RoboFriends/static/js/2.33001947.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8fa7a9693057c3426a69",
+    "url": "/RoboFriends/static/js/main.e8f70bc9.chunk.js"
+  },
+  {
+    "revision": "a763d911d8f6dec7a146",
+    "url": "/RoboFriends/static/js/runtime-main.4f72d9c4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
